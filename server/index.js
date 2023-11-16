1e4b7ac693ba8af8ca49a25466a6eaadd8cc536a
Denys <tymchenko.denys@gmail.com>
2023-11-16 20:25:42 +0200

    enchantments implemented (not final)

--- server/index.js
+++ server/index.js
@@ -13,9 +13,17 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
 const expressServer = app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-  console.log(`Open in browser: http://localhost:${PORT}/`)
+  console.log(`\nServer started on port ${PORT}`)
+  console.log(`Open in browser: http://localhost:${PORT}/\n`)
 })
+
+// state
+const usersState = {
+  users: [],
+  setUsers: function(newUsers) {
+    this.users = newUsers;
+  }
+}
 
 const io = new Server(expressServer, {
   cors: {
@@ -27,23 +35,88 @@ const io = new Server(expressServer, {
 
 io.on('connection', socket => {
   console.log(`User ${socket.id} connected`)
-  // On connection - for connected user only
-  socket.emit('message', 'Welcome to Chat App <3')
-  // On connection - for everyone, except user (because of broadcast)
-  socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`)
+  socket.emit('send-info-message', 'Type in your name and desired chat room to join conversation')
+
+  socket.on('join-room', ({ userName, room }) => {
+    // If user was connected to some room previously - disconect him from there
+    //const prevRoom = getUser(socket.id)?.room
+    //if (prevRoom) {
+    //  socket.leave(prevRoom)
+    //  io.to(prevRoom).emit('message', buildMsg(ADMIN, `${userName} has left the room`))
+    //}
+
+    const user = AddNewUserToTheRoom(socket.id, userName, room)
+    socket.join(user.room)
+    socket.emit('send-info-message', (`You have joined the ${user.room} chat room`))
+    socket.broadcast.to(user.room).emit('send-info-message', `${user.name} has joined the chat`)
+
+    // Update users list for chat room
+    io.to(user.room).emit('usersList', { users: getUsersInRoom(user.room) })
+
+    // Update users list for everyone
+    io.emit('roomsList', { rooms: getAllActiveRooms() })
+  })
 
   // Listening for events (emits)
-  socket.on('message', data => {
-    console.log(data)
-    io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
-  })
-
   socket.on('disconnect', () => {
-    console.log(`User ${socket.id} disconnected`);
-    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} disconnected`)
+    const user = getUser(socket.id)
+    if (!user) return
+
+    removeUser(socket.id)
+    io.to(user.room).emit('send-info-message', `${user.name} has left the chat`)
+    io.to(user.room).emit('usersList', { users: getUsersInRoom(user.room) })
+    io.emit('roomsList', { rooms: getAllActiveRooms() })
+
+    console.log(`${user.name} with socket id: ${socket.id} - disconnected`)
   });
 
-  socket.on('typing', (userName) => {
-    socket.broadcast.emit('typing', userName)
+  socket.on('send-message', ({ userName, message }) => {
+    const room = getUser(socket.id)?.room
+    if (!room) return
+
+    io.to(room).emit('send-message', buildMsg(userName, message))
   })
 })
+
+const buildMsg = (userName, text) => {
+  console.log('check')
+  return {
+    userName,
+    text,
+    time: new Intl.DateTimeFormat('default', {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+    }).format(new Date())
+  }
+}
+
+// User functions
+const AddNewUserToTheRoom = (id, name, room) => {
+  const user = { id, name, room }
+
+  usersState.setUsers([
+    ...usersState.users.filter(user => user.id !== id),
+    user
+  ])
+
+  return user
+}
+
+const removeUser = (id) => {
+  usersState.setUsers(
+    [...usersState.users.filter(user => user.id !== id)]
+  )
+}
+
+const getUser = (id) => {
+  return usersState.users.find(user => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+  return usersState.users.filter(user => user.room === room)
+}
+
+const getAllActiveRooms = () => {
+  return Array.from(new Set(usersState.users.map(user => user.room)))
+}
--- server/public/app.js
+++ server/public/app.js
@@ -1,39 +1,110 @@
 const socket = io('ws://localhost:3500')
 
-const messageInput = document.querySelector('input')
+const nameInput = document.getElementById('name')
+const chatRoomInput = document.getElementById('chat-room')
+const messageInput = document.getElementById('message')
+
+const chat = document.getElementById('chat')
+const chatMessages = chat.querySelector('.messages')
+
+const userList = document.getElementById('users')
+const roomList = document.getElementById('room')
+
 const isTypingMessage = document.querySelector('.is-typing-message')
 
 const sendMessage = (e) => {
   e.preventDefault()
 
-  if (messageInput.value) {
-    socket.emit('message', messageInput.value)
+  const canSendMessage = nameInput.value && chatRoomInput.value && messageInput.value
+
+  if (canSendMessage) {
+    socket.emit('send-message', {
+      userName: nameInput.value,
+      message: messageInput.value,
+    })
+
     messageInput.value = ''
   }
 }
 
+const joinRoom = (e) => {
+  e.preventDefault()
+  if (!nameInput.value && !chatRoomInput.value) return
+
+  socket.emit('join-room', {
+    userName: nameInput.value,
+    room: chatRoomInput.value,
+  })
+
+  chat.querySelector('header').innerText = `${chatRoomInput.value} Chat`
+}
+
 // Listen for message send
 document
-  .querySelector('form')
+  .getElementById('send-message')
   .addEventListener('submit', sendMessage)
 
-socket.on('message', (data) => {
-  isTypingMessage.innerText = ''
+document
+  .getElementById('join-chat')
+  .addEventListener('submit', joinRoom)
 
-  const message = document.createElement('li')
-  message.innerText = data
-  document.querySelector('.messages-list').append(message)
-})
-
-// Listen for message typing
 messageInput.addEventListener('keypress', () => {
-  socket.emit('typing', socket.id.substring(0, 5))
+  socket.emit('typing', nameInput.value)
 })
+
+socket.on('send-message', (data) => {
+  const { userName, time, text } = data
+
+  const message = document.createElement('div')
+  message.classList.add('message')
+  const isOunMessage = userName === nameInput.value
+  if(isOunMessage) message.classList.add('oun-message')
+
+
+  const header = createMessageHeader(userName, time)
+  const body = createMessageBody(text)
+  message.append(header, body)
+
+  chatMessages.append(message)
+})
+
+socket.on('send-info-message', (message) => {
+  const infoMessage = document.createElement('p')
+  infoMessage.classList.add('info-message')
+  infoMessage.innerText = message
+  chatMessages.append(infoMessage)
+})
+
 
 let isTypingMessageTimer = null
 socket.on('typing', userName => {
   isTypingMessage.innerText = `${userName} is typing...`
 
   clearTimeout(isTypingMessageTimer)
-  isTypingMessageTimer = setTimeout(() => {isTypingMessage.innerText = ''}, 1000)
+  isTypingMessageTimer = setTimeout(() => { isTypingMessage.innerText = '' }, 1000)
 })
+
+
+const createMessageHeader = (userName, time) => {
+  const messageHeader = document.createElement('div')
+  messageHeader.classList.add('header')
+
+  const usernameP = document.createElement('p')
+  usernameP.classList.add('username')
+  usernameP.innerText = userName
+
+  const timeP = document.createElement('p')
+  timeP.classList.add('time')
+  timeP.innerText = time
+
+  messageHeader.append(usernameP, timeP)
+  return messageHeader
+}
+
+const createMessageBody = (text) => {
+  const messageBody = document.createElement('div')
+  messageBody.classList.add('body')
+  messageBody.innerText = text
+
+  return messageBody
+}
